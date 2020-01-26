27463e936e997223698781fce2a1fc1f40cd35b8
bedoshi <bedoshidiary.ester.freesia@gmail.com>
2020-01-26 23:07:17 +0900

    Initial Commit

---
+++ SoundButtonApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SoundButtonApp
+//
+//  Created by Ester on 2020/01/26.
+//  Copyright © 2020 Bedoshi. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
